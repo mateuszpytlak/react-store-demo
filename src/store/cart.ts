5535eafbb1f00667081087462e41881225cdbb3d
Mateusz Pytlak <mateuszpytlak@gmail.com>
2025-10-20 14:31:02 +0200

    add: zustand cart state

---
+++ src/store/cart.ts
@@ -0,0 +1,33 @@
+import type {CartState} from "../types.ts";
+import {persist} from "zustand/middleware/persist";
+import {create} from "zustand/react";
+
+export const useCart = create<CartState>()(
+    persist(
+        (set, get) => ({
+            items: [],
+            add: (product, qty = 1) => set((state) => {
+                const existingItem = state.items.find(item => item.id === product.id)
+                if (existingItem) {
+                    return {
+                        items: state.items.map(item => item.id === product.id ? {...item, qty: item.qty + qty} : item)
+                    }
+                }
+                return {
+                    items: [{
+                        id: product.id,
+                        title: product.title,
+                        price: product.price,
+                        image: product.image,
+                        qty: qty,
+                    }, ...state.items]}
+            }),
+            remove: (id) => set( (state) => ( { items: state.items.filter(item => item.id !== id) })),
+            setQty: (id, qty) => set( (state) => ({ items: state.items.map( item => item.id === id ? {...item, qty } : item) })),
+            clear: () => set( { items: [] }),
+            totalItems: () => get().items.reduce((acc, item) => acc + item.qty, 0),
+            totalPrice: () => get().items.reduce((acc, item) => acc + item.qty * item.price, 0),
+        }),
+        {name: 'cart-storage'}
+    )
+)
--- src/types.ts
+++ src/types.ts
@@ -7,3 +7,21 @@ export type Product = {
   image: string;
   rating?: { rate: number; count: number };
 };
+
+export type CartItem = {
+    id: number;
+    title: string;
+    price: number;
+    image: string;
+    qty: number;
+}
+
+export type CartState = {
+    items: CartItem[];
+    add: (p: Product, qty?: number) => void;
+    remove: (id: number) => void;
+    setQty: (id: number, qty: number) => void;
+    clear: () => void;
+    totalItems: () => number;
+    totalPrice: () => number;
+}
