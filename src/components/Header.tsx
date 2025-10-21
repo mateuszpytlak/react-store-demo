fa532eb8d584f96118490ac1c7b54e35b265755e
Mateusz Pytlak <mateuszpytlak@gmail.com>
2025-10-21 12:35:30 +0200

    add: categories filtering

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,10 +9,10 @@ export const Header = () => {
             <div className="container flex items-center justify-between py-3 gap-4">
                 <Link to="/products" className="text-lg font-semibold">ReactStore</Link>
                 <nav className="flex items-center gap-4 text-sm">
-                    <NavLink to="/products"
-                             className={({isActive}) => isActive ? 'font-semibold' : ''}>Products</NavLink>
-                    <NavLink to="/cart" className={({isActive}) => isActive ? 'font-semibold' : ''}>Cart <span
-                        className="inline-flex items-center justify-center w-6 h-6 text-xs rounded-full bg-black text-white ml-1">{cartItemsCount}</span></NavLink>
+                    <NavLink to="/products" className={({isActive}) => isActive ? 'font-semibold' : ''}>Products</NavLink>
+                    <NavLink to="/cart" className={({isActive}) => isActive ? 'font-semibold' : ''}>
+                        Cart<span className="inline-flex items-center justify-center w-6 h-6 text-xs rounded-full bg-black text-white ml-1">{cartItemsCount}</span>
+                    </NavLink>
                 </nav>
             </div>
         </header>
