16043d5e6698f4362ee6625c606d117639b475d3
Mateusz Pytlak <mateuszpytlak@gmail.com>
2025-10-24 16:01:56 +0200

    add: testing dependencies and setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import {configDefaults} from "vitest/config";
 
 export default defineConfig({
     plugins: [
-        react(),       // 👈 obsługa JSX, TSX, Fast Refresh
-        tailwindcss(), // 👈 stylowanie Tailwind v4 (Oxide)
+        react(),
+        tailwindcss(),
     ],
+    test: {
+        environment: "jsdom",
+        globals: true,
+        setupFiles: "./src/setupTests.ts",
+        css: true,
+        coverage: {
+            reporter: ["text", "html"],
+            exclude: [...configDefaults.coverage?.exclude ?? [], "src/main.tsx"]
+        }
+    }
 });
